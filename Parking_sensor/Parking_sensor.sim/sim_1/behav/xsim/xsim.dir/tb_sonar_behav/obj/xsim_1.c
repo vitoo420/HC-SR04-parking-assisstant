036540ab5254fcb9f11f208a44ef2c87a4add570
Silvie2000 <78861112+Silvie2000@users.noreply.github.com>
2021-05-03 12:21:26 +0200

    merge

--- Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sonar_behav/obj/xsim_1.c
+++ Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sonar_behav/obj/xsim_1.c
@@ -56,17 +56,18 @@ typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
 IKI_DLLESPEC extern void execute_21(char*, char *);
 IKI_DLLESPEC extern void execute_22(char*, char *);
+IKI_DLLESPEC extern void execute_23(char*, char *);
 IKI_DLLESPEC extern void execute_20(char*, char *);
 IKI_DLLESPEC extern void transaction_0(char*, char*, unsigned, unsigned, unsigned);
 IKI_DLLESPEC extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[5] = {(funcp)execute_21, (funcp)execute_22, (funcp)execute_20, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
-const int NumRelocateId= 5;
+funcp funcTab[6] = {(funcp)execute_21, (funcp)execute_22, (funcp)execute_23, (funcp)execute_20, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
+const int NumRelocateId= 6;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/tb_sonar_behav/xsim.reloc",  (void **)funcTab, 5);
-	iki_vhdl_file_variable_register(dp + 2856);
-	iki_vhdl_file_variable_register(dp + 2912);
+	iki_relocate(dp, "xsim.dir/tb_sonar_behav/xsim.reloc",  (void **)funcTab, 6);
+	iki_vhdl_file_variable_register(dp + 4248);
+	iki_vhdl_file_variable_register(dp + 4304);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
--- Parking_sensor/Parking_sensor.srcs/sources_1/new/sensor.vhd
+++ Parking_sensor/Parking_sensor.srcs/sources_1/new/sensor.vhd
@@ -1,63 +1,65 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+
 entity sonar is
-    Port ( clk : in STD_LOGIC;
-           trig : out STD_LOGIC;--trigger
-           echo : in STD_LOGIC := '0';
-           cm1 : out unsigned(3 downto 0); --most significant bit of distance in binary form
-           cm0 : out unsigned(3 downto 0));
+    Port ( clk        : in STD_LOGIC;
+           trig       : out STD_LOGIC;    --trigger
+           echo       : in STD_LOGIC := '0';
+           cm_units_o : out unsigned(3 downto 0); --cm1
+           cm_tens_o  : out unsigned(3 downto 0)  --cm0
+           ); 
 end sonar;
-architecture sonar_arch of sonar is
-begin
+architecture Behavioral of sonar is
+    signal count       : integer range 0 to 10000;
+    signal tmpcm_units : unsigned(3 downto 0) := "0000";
+    signal tmpcm_tens  : unsigned(3 downto 0) := "0000";
+    signal sendTrig    : std_logic := '1';
+    signal echoPrev    : std_logic:= '0';
+    signal echoCurrent : std_logic:= '0';
+    begin
     process(clk)
-    variable count: integer range 0 to 10000;
-    variable tmpcm1: unsigned(3 downto 0) := "0000";
-    variable tmpcm0 : unsigned(3 downto 0) := "0000";
-    variable sendTrig: std_logic := '1';
-    variable echoPrev: std_logic:= '0';
-    variable echoCurrent : std_logic:= '0';
     begin
         if rising_edge(clk) then
             if sendTrig = '1' then -- send 10us trig
                 if count = 10 then -- 10 us trig is completed, reset tmp signals for the new calculation
                     trig <= '0';
-                    sendTrig := '0';
-                    count := 0;
-                    tmpcm1:= "0000";
-                    tmpcm0 := "0000";
+                    sendTrig <= '0';
+                    count <= 0;
+                    tmpcm_units <= "0000";
+                    tmpcm_tens <= "0000";
                 else
                     trig <= '1';
-                    count := count + 1;
+                    count <= count + 1;
                 end if; 
-            else -- stop sending trig, do the calculation (5800 clk cycle = 1cm)(count enters here as 0)
-                if echo = '1' then -- we can start calculating the width of the echo
-                    if count = 10 then --5799 then--we measuered 1 cm
-                        if tmpcm0 = "1001" then
-                           if tmpcm1 = "1001" then
-                                sendTrig := '1';--max distance to be measured(100 cm), stop measuring, re-send the trig(also update the output)
-                                cm0 <= tmpcm0;
-                                cm1 <= tmpcm1;
+            else -- stop sending trig, do the calculation (10 clk cycle = 1cm)
+                if echo = '1' then -- start counting
+                    if count = 10 then --10 measuered 1 cm
+                        if tmpcm_tens = "1001" then
+                           if tmpcm_units = "1001" then
+                                sendTrig <= '1';--max distance to be measured(cca 300 cm)g
+                                cm_tens_o <= tmpcm_tens;
+                                cm_units_o <= tmpcm_units;
                             else
-                                tmpcm1 := tmpcm1 + "0001";
-                                tmpcm0 := "0000";
+                                tmpcm_units <= tmpcm_units + "0001";
+                                tmpcm_tens <= "0000";
                             end if;
                         else
-                            tmpcm0 := tmpcm0 + "0001";
+                            tmpcm_tens <= tmpcm_tens + "0001";
                         end if;
-                        count := 0;
+                        count <= 0;
                     else
-                        count := count + 1;
+                        count <= count + 1;
                     end if;
-                    if echoPrev = '1' and echoCurrent = '0' then --falling edge of echo, edit the output, start sending new trigger
-                        sendTrig := '1';
-                        cm0 <= tmpcm0;
-                        cm1 <= tmpcm1;
+                    if echoPrev = '1' and echoCurrent = '0' then --falling edge of echo
+                        sendTrig <= '1';
+                        cm_tens_o <= tmpcm_tens;
+                        cm_units_o <= tmpcm_units;
                     end if;                              
                 end if;                        
             end if;
-            echoPrev := echoCurrent;
-            echoCurrent := echo;
+            echoPrev <= echoCurrent;
+            echoCurrent <= echo;
         end if;      
     end process; 
-end sonar_arch;
+end Behavioral;
