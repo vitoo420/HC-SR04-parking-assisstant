9ab78180f43dcd752ffb3d88e148fc26034985c1
Silvie2000 <78861112+Silvie2000@users.noreply.github.com>
2021-05-04 20:08:24 +0200

    megre

--- Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sensor_behav/webtalk/xsim_webtalk.tcl
+++ Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sensor_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir D:/Users/vitja/Documents/GitHub/DE1_projekt/Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sensor_behav/webtalk/
+webtalk_init -webtalk_dir D:/DE1_projekt/Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sensor_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue May  4 18:45:22 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue May  4 19:20:13 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -12,21 +12,21 @@ webtalk_add_data -client project -key target_family -value "not_applicable" -con
 webtalk_add_data -client project -key target_device -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_package -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
-webtalk_add_data -client project -key random_id -value "bb45780c-20f5-4f41-b9c2-8d41e92f6098" -context "software_version_and_target_device"
+webtalk_add_data -client project -key random_id -value "" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "f62276c9e7e3414287226c6ebc66759d" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "98" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "7" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
-webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-9300H CPU @ 2.40GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "2400 MHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-6700HQ CPU @ 2.60GHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "2592 MHz" -context "user_environment"
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
-webtalk_add_data -client project -key system_ram -value "16.000 GB" -context "user_environment"
+webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "5944_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 642685282 -regid "" -xml D:/Users/vitja/Documents/GitHub/DE1_projekt/Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sensor_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Users/vitja/Documents/GitHub/DE1_projekt/Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sensor_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Users/vitja/Documents/GitHub/DE1_projekt/Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sensor_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6396_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 185526636 -regid "" -xml D:/DE1_projekt/Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sensor_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/DE1_projekt/Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sensor_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/DE1_projekt/Parking_sensor/Parking_sensor.sim/sim_1/behav/xsim/xsim.dir/tb_sensor_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
